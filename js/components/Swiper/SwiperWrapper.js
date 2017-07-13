f6ecaa98aa2cd1736f47f57961419c84b6986a89
Ivan <wcy5211678@163.com>
2017-07-13 10:03:28 +0800

    swiper

--- js/components/Swiper/SwiperWrapper.js
+++ js/components/Swiper/SwiperWrapper.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  ViewPagerAndroid,
+  Platform,
+  ActivityIndicator,
+  StyleSheet
+} from 'react-native'
+
+const { width, height } = Dimensions.get('window')
 
 export default class SwiperWrapper extends Component {
   render () {
-    return (
+    console.log(Platform.OS, Dimensions.get('window'), Dimensions.get('screen'));
+    if (Platform.OS === 'ios') {
+      return (
+        <ScrollView style={styles.swiper}>
+          <Text style={styles.text}>ios swiper</Text>
+        </ScrollView>
+      )
+    }
 
+    return (
+      <ViewPagerAndroid>
+        <Text>android swiper</Text>
+      </ViewPagerAndroid>
     )
   }
 }
+
+var styles = StyleSheet.create({
+  swiper: {
+    backgroundColor: '#fffff0',
+    maxHeight: 50,
+    width: width,
+  },
+  text: {
+    fontSize: 10
+  }
+})
--- js/components/Swiper/index.js
+++ js/components/Swiper/index.js
@@ -8,20 +8,12 @@ import {
   StyleSheet
  } from 'react-native'
 import PropTypes from 'prop-types' // 属性枚举
+import SwiperWrapper from './SwiperWrapper'
 
 export default class extends Component {
   render () {
     return (
-      <ScrollView style={styles.swiper}>
-        <View><Text>Hello Swiper!!</Text></View>
-      </ScrollView>
+      <SwiperWrapper></SwiperWrapper>
     )
   }
 }
-
-var styles = StyleSheet.create({
-  swiper: {
-    backgroundColor: 'red',
-    maxHeight: 50
-  }
-})
