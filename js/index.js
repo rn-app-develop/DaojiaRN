d53c5214a2713649522a671055b571c7af297c24
Ivan <wcy5211678@163.com>
2017-07-18 15:41:18 +0800

    add router

--- js/index.js
+++ js/index.js
@@ -11,89 +11,38 @@ import {
   View
 } from 'react-native'
 
-import PropTypes from 'prop-types' // 属性枚举
-import Swiper from './components/Swiper'
+import {
+  Scene,
+  Router,
+  Actions,
+  Reducer,
+  ActionConst
+} from 'react-native-router-flux'
 
-export default class DaojiaRN extends Component {
-  static propTypes = {
-    horizontal: PropTypes.bool
-  }
+import Home from './pages/Home' // 属性枚举
+import Login from './pages/Login' // 属性枚举
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! Welcome to Daojia!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit js/index.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Swiper style={styles.wrapper} height={240}  horizontal={true} autoplay>
-          <View style={styles.slide1}>
-           <Text style={styles.text}>Hello Swiper</Text>
-         </View>
-         <View style={styles.slide2}>
-           <Text style={styles.text}>Beautiful</Text>
-         </View>
-         <View style={styles.slide3}>
-           <Text style={styles.text}>And simple</Text>
-         </View>
-        </Swiper>
-      </View>
-    )
+// 创建状态管理器 reducer
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params)
+  return (state, action) => {
+    console.log('Action:', action)
+    return defaultReducer(state, action)
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    padding: 20,
-    backgroundColor: '#ff0'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  wrapper: {
-  },
+export default class DaojiaRN extends Component {
 
-  slide: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
-  },
-
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB'
-  },
-
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5'
-  },
-
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
-  },
-});
+  render() {
+    return (
+      <Router createReducer={reducerCreate} tintColor='red'>
+        <Scene
+          leftButtonTextStyle={{color: 'green'}}
+          backButtonTextStyle={{color: 'red'}}>
+          <Scene key='home' component={Home} title='Home' />
+          <Scene key='login' component={Login} title='Login'/>
+        </Scene>
+      </Router>
+    )
+  }
+}
