53b0fa4e6d01f96689d2ad422f59191316904978
yanchengqu <yanchengqu@126.com>
2017-08-09 17:14:05 +0800

    api

--- js/api/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const getStoreHome = (data) => {
-    return axios.get(`store/storeDetailV220?storeId=${data.storeId}&skuId=${data.skuId}&activityId=${data.activityId}&promotionType=${promotionType}&longitude=${longitude}&latitude={latitude}`)
-}
