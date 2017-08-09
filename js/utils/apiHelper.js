607f144186cfcba7ea11bbf95544cc84fa587f1e
Ivan <wcy5211678@163.com>
2017-08-09 16:18:04 +0800

    api helper

---
+++ js/utils/apiHelper.js
@@ -0,0 +1,119 @@
+import axios from 'axios';
+
+// let baseURL = 'https://pregw.o2o.jd.com/client' // 第一套预发布
+// let baseURL = 'http://testgw.o2o.jd.com/client' // 第二套预发布
+// let baseURL = 'https://gw.o2o.jd.com/client' // 线上环境
+let baseURL = 'http://gw.o2o.jd.com/client';
+// let baseURL = 'http://daojia.jd.com/client'
+// let baseURL = 'http://testpdjm.jd.com/client'
+let indexParams = {
+  functionId:'indexh5/getIndex',
+  body:{
+    "address":"朝林广场",
+    "city":"北京市",
+    "longitude":116.50628,
+    "latitude":39.79311,
+    "coordType":"2"
+  },
+  // platCode:'H5',
+  // appVersion:'4.2.0',
+  // appName:'paidaojia',
+  lng:116.50628,
+  lat:39.79311,
+  city_id:1
+};
+export const getIndex = function () {
+  return axios.getAPI(indexParams)
+};
+
+let recommendParams = {
+  functionId:'zone/recommendStoreList',
+  body:{
+    "channelId":"",
+    "city":"北京市",
+    "longitude":116.50628,
+    "latitude":39.79311,
+    "currentPage":1,
+    "pageSize":10,
+    "areaCode":1,
+    "coordType":"2",
+    "platform":"1"
+  },
+  // platCode:'H5',
+  // appVersion:'4.2.0',
+  // appName:'paidaojia',
+  lng:116.50628,
+  lat:39.79311,
+  city_id:1,
+};
+
+export const recommendStoreList = function (currentPage) {
+  recommendParams.body.currentPage = currentPage
+  return axios.getAPI(recommendParams)
+};
+
+let activityParams = {
+  functionId:'act/getActivityFirst',
+  body:{
+    // "longitude":116.37335,
+    "longitude":116.50628,
+    // "latitude":39.92122,
+    "latitude":39.79311,
+    "activityId":"33327",
+    "city":"北京市",
+    "shareFlag":false,
+    "coordType":2,
+    "currentPage":1,
+  },
+  // lng:116.50628, // 朝林广场
+  lng:116.37335,  // 那间咖啡
+  // lat:39.79311, // 朝林广场
+  lat:39.92122, // 那间咖啡
+  city_id:1,
+};
+
+export const getActivity = function (activityId, currentPage) {
+  activityParams.body.currentPage = currentPage
+  activityParams.body.activityId = activityId
+  // return axios.get(baseURL, {params: activityParams})
+  return axios.getAPI(activityParams)
+};
+
+let storeParams = {
+  functionId:'store/storeDetailV220',
+  body:{
+    "ref":"index/LID:11",
+    "storeId":"10003450",
+    "skuId":"",
+    "activityId":"",
+    "promotionType":"",
+    "longitude":116.50628,
+    "latitude":39.79311,
+  },
+  // appVersion:'4.2.0',
+  // appName:'paidaojia',
+  // platCode:'H5',
+  lng:116.50628,
+  lat:39.79311,
+  city_id:1,
+};
+
+export const getStoreDetail = function (storeId) {
+  storeParams.body.storeId = storeId;
+  // return axios.get(baseURL, {params: storeParams});
+  return axios.getAPI(storeParams);
+};
+
+// 根据经纬度获取地址信息
+export const getAddress = (coords) => {
+  let params = {
+    functionId: 'local/getAddressN',
+    body: {
+      coord_type: '2',
+      needMatch: 0,
+      ...coords
+    }
+  }
+
+  return axios.getAPI(params)
+}
---
+++ js/utils/interceptors.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+// const commonParams = {platCode: 'H5', appName: 'paidaojia', appVersion: '4.4.0'}
+// 公共参数
+const commonParams = {platCode: 'H5', appName: 'paidaojia', appVersion: '4.4.0'}
+// const url = '/client'
+// const baseURL = 'https://pregw.o2o.jd.com/client' // 第一套预发布
+// const baseURL = 'http://testgw.o2o.jd.com/client' // 第二套预发布
+// const baseURL = 'https://gw.o2o.jd.com/client' // 线上环境
+const baseURL = 'http://gw.o2o.jd.com/client';
+// const baseURL = 'https://daojia.jd.com/client'
+// const baseURL = 'http://testpdjm.jd.com/client'
+
+// axios.defaults.baseURL = baseURL;
+
+axios.getAPI = axios.prototype.getAPI = (params) => {
+  return axios.get(baseURL, {params: params})
+}
+
+axios.postAPI = axios.prototype.postAPI = (params) => {
+  return axios.post(baseURL, params)
+}
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  // console.log(config);
+  config.params = {
+    ...config.params,
+    ...commonParams
+  }
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response.data;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
