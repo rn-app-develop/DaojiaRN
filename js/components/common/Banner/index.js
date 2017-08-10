c726e82da17643cf305588e7538bd262e3b4c58a
Ivan <wcy5211678@163.com>
2017-08-10 15:40:23 +0800

    banner

--- js/components/common/Banner/index.js
+++ js/components/common/Banner/index.js
@@ -2,31 +2,14 @@ import React from 'react'
 
 import {
   View,
+  Text,
   StyleSheet,
+  Dimensions
 } from 'react-native'
-
-import {
-  JDDevice,
-  JDRouter,
-  JDTouchable,
-  JDImage,
-  JDJumping,
-  JDViewPager
-} from 'jdreact-core-lib'
-
-const {
-  Router,
-  Route,
-} = JDRouter
-
 import PropTypes from 'prop-types'
 
-const getDataSource = (data) => {
-  let dataSource = new JDViewPager.DataSource({
-    pageHasChanged: (p1, p2) => p1 !== p2,
-  });
-  return dataSource.cloneWithPages(data)
-}
+const { width, height } = Dimensions.get('window')
+import Swiper from '../Swiper'
 
 export default class Banner extends React.Component {
 
@@ -46,66 +29,47 @@ export default class Banner extends React.Component {
     let gap = this.props.firstBanner ? 0 : 10;
 
     return (
-      <JDViewPager
-        style={{
-          width: JDDevice.width,
-          overflow: 'hidden',
-          justifyContent: 'center',
-          backgroundColor: '#fff',
-          marginTop: gap,
-          marginBottom: gap,
-          paddingLeft: gap,
-          paddingRight: gap,
-          height: this.props.firstBanner ? JDDevice.width * 360 / 750 : JDDevice.width * 232 / 710
-        }}
-        dataSource={getDataSource(this.props.dataSource)}
-        renderPage={this._renderPage.bind(this)}
-        isLoop={true}
-        autoPlay={this.props.firstBanner}
-        onChangePage={this._onChangePage.bind(this)}
-      />
-    );
-  }
 
-  _renderPage(data) {
-    return (
-      <JDTouchable style={styles.bannerItem} onPress={() => {
-        // alert('点击图片' + data.linkUrl)
-        if (data.floorCellData.params.url) {
-          JDJumping.jumpToWeb( data.floorCellData.params.url ).catch(err => alert( err ))
-        } else {
-          alert(`toDj-${data.floorCellData.to}`)
-          this.context.router[`toDj-${data.floorCellData.to}`](
-            data.floorCellData.params
-          )
-        }
-      }} >
-        <JDImage style={{
-            flex: 1,
-            resizeMode: 'stretch',
-            height: this.props.firstBanner ? JDDevice.width * 360 / 750 : JDDevice.width * 232 / 710,
-            width: this.props.firstBanner ? JDDevice.width : JDDevice.width - 20,
-          }} source={{uri: data.floorCellData.imgUrl}} />
-      </JDTouchable>
+      <Swiper style={styles.wrapper}
+        height={this.props.firstBanner ? width * 360 / 750 : width * 232 / 710}
+        horizontal={true} autoplay>
+        <View style={styles.slide1}>
+         <Text style={styles.text}>Hello Swiper</Text>
+       </View>
+       <View style={styles.slide2}>
+         <Text style={styles.text}>Beautiful</Text>
+       </View>
+       <View style={styles.slide3}>
+         <Text style={styles.text}>And simple</Text>
+       </View>
+      </Swiper>
     );
-  }
-
-  _onChangePage(pageNumber) {
-    this.setState({
-      pageNow: pageNumber
-    });
   }
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    marginBottom: JDDevice.getRpx(40),
-  },
-  viewPager: {
-    width: JDDevice.width,
-    overflow: 'hidden',
+    marginBottom: 20,
   },
   bannerItem: {
     flex: 1
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
   },
 });
--- js/pages/TabView/Home.js
+++ js/pages/TabView/Home.js
@@ -12,7 +12,8 @@ import {
 } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import Button from 'react-native-button'
-import Swiper from '../../components/common/Swiper'
+// import Swiper from '../../components/common/Swiper'
+import Banner from '../../components/common/Banner'
 
 export default class Home extends Component {
 
@@ -31,17 +32,7 @@ export default class Home extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
-        <Swiper style={styles.wrapper} height={240}  horizontal={true} autoplay>
-          <View style={styles.slide1}>
-           <Text style={styles.text}>Hello Swiper</Text>
-         </View>
-         <View style={styles.slide2}>
-           <Text style={styles.text}>Beautiful</Text>
-         </View>
-         <View style={styles.slide3}>
-           <Text style={styles.text}>And simple</Text>
-         </View>
-        </Swiper>
+        <Banner></Banner>
       </View>
     )
   }
@@ -65,34 +56,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
-  wrapper: {
-  },
-
-  slide: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
-  },
-
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB'
-  },
-
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5'
-  },
-
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
   },
 });
