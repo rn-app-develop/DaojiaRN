e88606e369af837e295e10503a5a0e97a6c23c91
Ivan <wcy5211678@163.com>
2017-08-15 14:54:12 +0800

    nav

--- js/components/common/TabView/index.js
+++ js/components/common/TabView/index.js
@@ -1,1 +1,5 @@
 import React, { Component } from 'react'
+
+import nav  from '../../../assets/nav4.png'
+
+console.log(nav)
--- js/index.js
+++ js/index.js
@@ -27,7 +27,9 @@ import Icon from 'react-native-vector-icons/Entypo';
 import './utils/interceptors'
 
 const myIcon = (<Icon name="back" size={20} color="#900" />)
+import nav from './assets/nav4.png'
 
+console.log(nav)
 // 创建状态管理器 reducer
 const reducerCreate = params => {
   const defaultReducer = new Reducer(params)
