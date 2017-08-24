3cbbc2f920a647bfb46e8e0fbb638afe79f9e8e8
Ivan <wcy5211678@163.com>
2017-08-24 16:59:26 +0800

    expanded

--- app/Pages/SettlementView/SettleContent/ProductList.js
+++ app/Pages/SettlementView/SettleContent/ProductList.js
@@ -4,12 +4,43 @@ import {
   Text,
   StyleSheet,
   Animated,
+  TouchableWithoutFeedback
 } from 'react-native';
 import ProductItem from './ProductItem'
 import ContentText from './ContentText'
 import IconArrow from '../IconArrow'
 
 export default class ProductList extends Component {
+
+  constructor (props) {
+    super(props)
+    let size = this.props.productInfo.length
+    let minHeight = size >= 2 ? 140 : size * 70
+    let maxHeight = this.props.productInfo.length * 70
+    this.state = {
+      height: new Animated.Value(minHeight),
+      expanded: false,
+      minHeight: minHeight,
+      maxHeight: maxHeight
+    }
+  }
+
+  toggle () {
+    let state = this.state
+    let finalHeight = state.expanded ? state.minHeight : state.maxHeight
+    this.setState({
+      expanded: !state.expanded
+    })
+
+    Animated.timing(
+      this.state.height,
+      {
+        toValue: finalHeight,
+        duration: 500
+      }
+    ).start();
+  }
+
   render() {
     let list = this.props.productInfo.map((val) => {
       return (
@@ -18,15 +49,18 @@ export default class ProductList extends Component {
       )
     })
     return (
-      <Animated.View>
-        <View>
+      <View>
+        <Animated.View style={{overflow: 'hidden', height: this.state.height}}>
           {list}
-        </View>
-        <View style={styles.totalInfo}>
-          <ContentText style={{color: '#888'}}>共{list.length}件/{this.props.totalWeight}kg</ContentText>
-          <IconArrow direction='bottom' style={styles.arrow}></IconArrow>
-        </View>
-      </Animated.View>
+        </Animated.View>
+        <TouchableWithoutFeedback
+          onPress={this.toggle.bind(this)}>
+          <View style={styles.totalInfo}>
+            <ContentText style={{color: '#888'}}>共{list.length}件/{this.props.totalWeight}kg</ContentText>
+            <IconArrow direction={this.state.expanded ? 'top' : 'bottom'} style={styles.arrow}></IconArrow>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
     );
   }
 }
